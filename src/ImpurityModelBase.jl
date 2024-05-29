c04f85440c392dfd415faf9052ea8b481650f85c
guo chu <guochu604b@gmail.com>
2024-05-29 15:26:39 +0800

    gc changes

--- src/ImpurityModelBase.jl
+++ src/ImpurityModelBase.jl
@@ -2,7 +2,7 @@ module ImpurityModelBase
 
 # definition of bath, spectrum density
 export AbstractFermionicBath, FermionicBath, FermionicVacuum, fermionicbath, thermaloccupation
-export SpectrumFunction, lowerbound, upperbound
+export SpectrumFunction, lowerbound, upperbound, semicircular
 
 # Fourier transformations
 export Gt_to_Gw, Gt_to_Δw, Aw_to_Gτ, Gw_to_Aw, frequencies
@@ -10,7 +10,7 @@ export Gτ_to_Giw, Giw_to_Gτ, Δτ_to_Δiw, ifrequency, ifrequencies
 
 # analytical solutions 
 export free_greater, free_lesser, free_Gt, free_Gτ
-export toulouse_Gw, toulouse_Gt
+export toulouse_Gw, toulouse_Gt, toulouse_Δw, toulouse_Aw
 export toulouse_Giw, toulouse_Gτ, toulouse_Δiw, toulouse_Δτ
 
 # utilities
--- src/spectrumfunc.jl
+++ src/spectrumfunc.jl
@@ -32,3 +32,46 @@ function semicircular(t::Real)
 	return SpectrumFunction(ϵ -> sqrt(1-(ϵ/D)^2) * (D/π), lb = -D, ub = D)
 end
 semicircular(; t::Real=1) = semicircular(t)
+
+
+
+# wrapper a discrete array into a continuous function
+
+"""
+	struct ContinuousFuncWrapper{T<:Number}
+	omegas are discretized frequencies, fs are the functions values at the middle points.
+"""
+struct ContinuousFuncWrapper{T<:Number}
+	ws::Vector{Float64}
+	fs::Vector{T}
+
+function ContinuousFuncWrapper{T}(ws::Vector{<:Real}, fs::AbstractVector{<:Number}) where {T <: Number}
+	(length(ws) == length(fs)) || throw(ArgumentError("Number of frequencies must be equal to the number of function values"))
+	(ws == sort(ws)) || throw(ArgumentError("frequencies must be sorted from small to large"))
+	new{T}(convert(Vector{Float64}, ws), convert(Vector{T}, fs))
+end
+
+end
+
+ContinuousFuncWrapper(ws::Vector{<:Real}, fs::AbstractVector{T}) where {T<:Number} = ContinuousFuncWrapper{T}(ws, fs)
+
+function (x::ContinuousFuncWrapper)(ω::Real)
+	((ω < freqs_min(x)) || (ω > freqs_max(x))) && return zero(eltype(x.fs))
+	(ω == x.ws[end]) && return x.fs[end]
+	i = 0
+	for pos in 1:(length(x.fs)-1)
+		if (ω >= x.ws[pos]) && (ω < x.ws[pos+1])
+			i = pos
+			break;
+		end
+	end
+	(i == 0) && error("something wrong.")
+	dif = (ω - x.ws[i]) / (x.ws[i+1] - x.ws[i])
+	return x.fs[i] * (1 - dif) + x.fs[i+1] * dif
+end
+
+
+freqs_min(x::ContinuousFuncWrapper) = x.ws[1]
+freqs_max(x::ContinuousFuncWrapper) = x.ws[end]
+
+SpectrumFunction(ws::Vector{<:Real}, fs::AbstractVector{<:Number}) = SpectrumFunction(ContinuousFuncWrapper(ws, fs), lb=ws[1], ub=ws[end])
