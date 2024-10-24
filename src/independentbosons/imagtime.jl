fc55340f50641baedc32e2a66e08c8f15536efec
guo chu <guochu604b@gmail.com>
2024-10-24 13:43:50 +0800

    gc changes

--- src/independentbosons/imagtime.jl
+++ src/independentbosons/imagtime.jl
@@ -6,7 +6,7 @@ Matsubara Green's function in the imaginary-time axis
 """
 function independentbosons_Gτ(spectrum::SpectrumFunction, τ::Real; β::Real, ϵ_d::Real, U::Real=0, Δ::Real=_compute_Δ(spectrum))
     μ = -ϵ_d
-    return (U == 0) ? -(1-freefermion_occupation(β, μ))*exp(τ*(μ+Δ))*_exponent_f(spectrum, τ, β) : -_interact(τ, β, μ, Δ, U)*_exponent_f(spectrum, τ, β)
+    return (U == 0) ? (1-freefermion_occupation(β, μ))*exp(τ*(μ+Δ))*_exponent_f(spectrum, τ, β) : _interact(τ, β, μ, Δ, U)*_exponent_f(spectrum, τ, β)
 end
 function independentbosons_Gτ(spectrum::SpectrumFunction; β::Real, N::Int, ϵ_d::Real, U::Real=0)
     Δ = _compute_Δ(spectrum)
