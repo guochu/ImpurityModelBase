25017a8f04b305983543991e0c405ec9ab46b362
guo chu <guochu604b@gmail.com>
2024-10-26 17:49:45 +0800

    gc changes

--- src/independentbosons/imagtime.jl
+++ src/independentbosons/imagtime.jl
@@ -4,17 +4,23 @@
 
 Matsubara Green's function in the imaginary-time axis
 """
-function independentbosons_Gτ(spectrum::SpectrumFunction, τ::Real; β::Real, ϵ_d::Real, U::Real=0, Δ::Real=_compute_Δ(spectrum))
+function independentbosons_Gτ(spectrum::SpectrumFunction, τ::Real; β::Real, ϵ_d::Real, U::Real=0, nbands::Int=1, Δ::Real=_compute_Δ(spectrum))
+    (nbands in (1, 2)) || throw(ArgumentError("nbands must be 1 or 2"))
     μ′ = -ϵ_d + Δ
-    return (U == 0) ? (1-freefermion_occupation(β, μ′))*exp(τ*μ′)*_exponent_f(spectrum, τ, β) : _interact(τ, β, μ′, U)*_exponent_f(spectrum, τ, β)
+    if nbands == 1
+        (U == 0) || println("nonzero U=$(U) ignored for nbands=1")
+        return (1-freefermion_occupation(β, μ′))*exp(τ*μ′)*_exponent_f(spectrum, τ, β)
+    else
+        return _interact(τ, β, -ϵ_d, Δ, U)*_exponent_f(spectrum, τ, β)
+    end
 end
-function independentbosons_Gτ(spectrum::SpectrumFunction; β::Real, N::Int, ϵ_d::Real, U::Real=0)
+function independentbosons_Gτ(spectrum::SpectrumFunction; β::Real, N::Int, ϵ_d::Real, U::Real=0, nbands::Int=1)
     Δ = _compute_Δ(spectrum)
     δτ = β / N
     gτ = zeros(Float64, N+1)
     for i in 1:N
         τ = (i-1) * δτ
-        tmp = independentbosons_Gτ(spectrum, τ, β=β, ϵ_d=ϵ_d, U=U, Δ=Δ)
+        tmp = independentbosons_Gτ(spectrum, τ, β=β, ϵ_d=ϵ_d, U=U, Δ=Δ, nbands=nbands)
         (abs(imag(tmp)) < 1.0e-8) || error("imaginary part of Gτ is too large")
         gτ[i] = real(tmp)
     end
--- src/independentbosons/realtime.jl
+++ src/independentbosons/realtime.jl
@@ -4,34 +4,38 @@
 Retarded Green's function in the real time axis
 """
 function independentbosons_greater(spectrum::SpectrumFunction, t::Real; β::Real, ϵ_d::Real, U::Real=0, 
-                                    n₀::Union{Real, Nothing}=nothing, Δ::Real=_compute_Δ(spectrum))
+                                    n₀::Union{Real, Nothing}=nothing, nbands::Int=1, Δ::Real=_compute_Δ(spectrum))
+    (nbands in (1, 2)) || throw(ArgumentError("nbands must be 1 or 2"))
     μ′ = -ϵ_d + Δ
     r::ComplexF64 = 0
-    if U == 0
+    if nbands == 1
+        (U == 0) || println("nonzero U=$(U) ignored for nbands=1")
         if isnothing(n₀)
             n₀ = freefermion_occupation(β, μ′)
         end
         r = -im*(1-n₀)*exp(im*t*μ′)*_exponent_f(spectrum, im*t, β)
     else
         isnothing(n₀) || println("only local thermal initial state is supported for interacting case, n₀ is ignored")
-        r = -im*_interact(im*t, β, μ′, U)*_exponent_f(spectrum, im*t, β)
+        r = -im*_interact(im*t, β, -ϵ_d, Δ, U)*_exponent_f(spectrum, im*t, β)
     end
     return r
 end
 
 
 function independentbosons_lesser(spectrum::SpectrumFunction, t::Real; β::Real, ϵ_d::Real, U::Real=0,
-                                   n₀::Union{Real, Nothing}=nothing, Δ::Real=_compute_Δ(spectrum))
+                                   n₀::Union{Real, Nothing}=nothing, nbands::Int=1, Δ::Real=_compute_Δ(spectrum))
+    (nbands in (1, 2)) || throw(ArgumentError("nbands must be 1 or 2"))
     μ′ = -ϵ_d + Δ
     r::ComplexF64 = 0
-    if U == 0
+    if nbands == 1
+        (U == 0) || println("nonzero U=$(U) ignored for nbands=1")
         if isnothing(n₀)
             n₀ = freefermion_occupation(β, μ′)
         end
         r = im*n₀*exp(im*t*μ′)*_exponent_f(spectrum, im*t, β)
-    else  
+    else               
         isnothing(n₀) || println("only local thermal initial state is supported for interacting case, n₀ is ignored")
-        r = im*_interact(im*t, β, μ′, U)*_exponent_f(spectrum, im*t, β)
+        r = im*_interact(im*t, β, -ϵ_d, Δ, U)*_exponent_f(spectrum, im*t, β)        
     end
     return r 
 end
