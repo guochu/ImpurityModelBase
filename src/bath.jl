1ea2d9365250814dad9cd60899d312239874b2a0
guo chu <guochu604b@gmail.com>
2025-04-06 11:27:28 +0800

    minor changes

--- src/bath.jl
+++ src/bath.jl
@@ -71,18 +71,22 @@ const AbstractFermionicBath = Union{FermionicBath{F}, FermionicVacuum{F}} where 
 
 Boson-einstein distribution for a bosonic bath 
 with β, μ at energy ϵ
+μ = 0 by default
 """
-function boseeinstein(β::Real, μ::Real, ϵ::Real)
-	(ϵ > μ) || throw(ArgumentError("energy must be larger than μ"))
-	x = exp(-safe_mult(β, ϵ - μ))
+function boseeinstein(β::Real, ϵ::Real)
+	(ϵ > 0) || throw(ArgumentError("energy must be larger than μ"))
+	x = exp(-safe_mult(β, ϵ))
 	return x / (1 - x)
 end
+
+boseeinstein(β::Real, μ::Real, ϵ::Real) = boseeinstein(β, ϵ - μ)
 
 # function boseeinstein(β::Real, μ::Real, ϵ::Real)
 # 	(ϵ > μ) || throw(ArgumentError("energy must be larger than μ"))
 # 	return 1 / (exp(safe_mult(β, ϵ - μ)) - 1)
 # end
 thermaloccupation(::Type{Boson}, β::Real, μ::Real, ϵ::Real) = boseeinstein(β, μ, ϵ)
+thermaloccupation(::Type{Boson}, β::Real, ϵ::Real) = boseeinstein(β, ϵ)
 
 """
 	fermidirac(β, μ, ϵ)
@@ -90,20 +94,23 @@ thermaloccupation(::Type{Boson}, β::Real, μ::Real, ϵ::Real) = boseeinstein(β
 Return Fermi-Dirac distribution for a fermionic bath 
 with β, μ at energy ϵ
 """
-function fermidirac(β::Real, μ::Real, ϵ::Real)
-	if ϵ >= μ
-		x = exp(-safe_mult(β, ϵ-μ))
+function fermidirac(β::Real, ϵ::Real)
+	if ϵ >= 0
+		x = exp(-safe_mult(β, ϵ))
 		return x/(1+x)
 	else
-		return 1.0/(1.0+exp(safe_mult(β, ϵ-μ)))
+		return 1.0/(1.0+exp(safe_mult(β, ϵ)))
 	end
 end
+fermidirac(β::Real, μ::Real, ϵ::Real) = fermidirac(β, ϵ-μ)
+
 
 # function fermidirac(β::Real, μ::Real, ϵ::Real)
 # 	x = exp(-safe_mult(β, ϵ-μ))
 # 	return x/(1+x)
 # end
 thermaloccupation(::Type{Fermion}, β::Real, μ::Real, ϵ::Real) = fermidirac(β, μ, ϵ)
+thermaloccupation(::Type{Fermion}, β::Real, ϵ::Real) = fermidirac(β, ϵ)
 
 """
 	thermaloccupation(bath::AbstractBosonicBath, energy::Real)
