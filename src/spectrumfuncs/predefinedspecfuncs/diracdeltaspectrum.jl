68cfeef96ba4cfaa349297696236559a065c46cb
guo chu <guochu604b@gmail.com>
2025-04-08 20:02:20 +0800

    gc changes

--- src/spectrumfuncs/predefinedspecfuncs/diracdeltaspectrum.jl
+++ src/spectrumfuncs/predefinedspecfuncs/diracdeltaspectrum.jl
@@ -9,10 +9,11 @@ struct DiracDelta <: AbstractSpectrumFunction
 	lb::Float64
 	ub::Float64
 end
-function DiracDelta(;ω::Real=0, α::Real=1, lb::Real=-Inf, ub::Real=Inf)
+function DiracDelta(ω::Real; α::Real=1, lb::Real=-Inf, ub::Real=Inf) 
 	(lb < ub) || throw(ArgumentError("lb must be less than ub"))
 	return DiracDelta(convert(Float64, ω), convert(Float64, α), convert(Float64, lb), convert(Float64, ub))
 end
+DiracDelta(;ω::Real=0, α::Real=1, lb::Real=-Inf, ub::Real=Inf) = DiracDelta(ω, α=α, lb=lb, ub=ub)
 (x::DiracDelta)(ϵ::Real) = error("can not call to a delta function")
 
 quadgkwrapper(f::DiracDelta) = ifelse(lowerbound(f) <= f.ω <= upperbound(f), f.α, 0.)
